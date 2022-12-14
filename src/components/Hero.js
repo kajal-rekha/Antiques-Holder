b700b11fd89fd9b0b28f09f6682b892c76b917bb
kajal-2011 <kajalrekha888@gmail.com>
2022-12-15 03:38:20 +0600

    project done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,8 +9,10 @@ const Hero = () => {
           "url(https://www.onderdepoort.be/responsive-images/brocante-back/bg-wide-jpg/back-brocante-wide_foqmmu_c_scale%2Cw_1741.jpg)",
       }}
     >
-      <h1 className="antique">Golden Era</h1>
-      <h1 className="collectibles">Antigues</h1>
+      <h1 className="antique">
+        Golden
+        <span style={{ color: "#e74c3c" }}>Era</span>{" "}
+      </h1>
     </section>
   );
 };
