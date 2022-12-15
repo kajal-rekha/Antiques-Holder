161fce273c247d2dec5234ac922408ead2d6947c
kajal-2011 <kajalrekha888@gmail.com>
2022-12-15 18:12:26 +0600

    project done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,17 @@
-import { useRef } from "react";
-
 const Hero = () => {
   return (
     <section
-      className="hero wrapper"
+      className="hero wrapper overlay"
       style={{
         backgroundImage:
-          "url(https://www.onderdepoort.be/responsive-images/brocante-back/bg-wide-jpg/back-brocante-wide_foqmmu_c_scale%2Cw_1741.jpg)",
+          "url(https://cdn.shopify.com/s/files/1/0061/4924/2969/files/antique-furniture-table-banner_1800x800.jpg?v=1613641840)",
       }}
     >
       <h1 className="antique">
         Golden
-        <span style={{ color: "#e74c3c" }}>Era</span>{" "}
+        <span style={{ color: "#e74c3c" }}>Era</span>
       </h1>
+      <h1 className="antique">Antiques</h1>
     </section>
   );
 };
